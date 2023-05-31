692f718c9fc359246880ba048612144676e5434d
Oswaldo <oswaldoj.tipan@gmail.com>
2023-05-31 12:44:19 -0500

    HomeSchoolStudent Class added

---
+++ 06-Code/MagamentSystem/src/ec/edu/espe/managmentsystem/model/HomeSchoolStudent.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.managmentsystem.model;
+
+/**
+ *
+ * @author Oswaldo Tipan
+ */
+public class HomeSchoolStudent {
+    private int id;
+    private int age;
+    private String name;
+    private String typeOfStudy;
+    private String address;    
+}
