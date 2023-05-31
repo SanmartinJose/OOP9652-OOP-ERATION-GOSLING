e2ef1dbfd275c55f0c35a67f01b2df18f372f4d2
SanmartinJose <132719688+SanmartinJose@users.noreply.github.com>
2023-05-31 13:28:54 -0500

    Student Class

--- 06-Code/MagamentSystem/src/ec/edu/espe/managmentsystem/model/HomeSchoolStudent.java
+++ 06-Code/MagamentSystem/src/ec/edu/espe/managmentsystem/model/HomeSchoolStudent.java
@@ -1,8 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ec.edu.espe.managmentsystem.model;
+
+import java.util.Date;
 
 /**
  *
@@ -13,5 +12,77 @@ public class HomeSchoolStudent {
     private int age;
     private String name;
     private String typeOfStudy;
-    private String address;    
+    private String address; 
+    private Date bornDate;
+
+    public HomeSchoolStudent(int id, String name, String typeOfStudy, String address, Date bornDate) {
+        this.id = id;
+        this.name = name;
+        this.typeOfStudy = typeOfStudy;
+        this.address = address;
+        this.bornDate = bornDate;
+        bornDate.setYear(bornDate.getYear()-1900);
+        this.age=computeAge().getYear();
+    }
+    private Date computeAge(){
+        // TODO code, compute agein years using the bornOnDate
+        long timeDiff = new Date().getTime() - bornDate.getTime();
+        Date ageDate = new Date();
+        ageDate.setTime(timeDiff);
+        ageDate.setYear(ageDate.getYear()-70);
+        return ageDate;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTypeOfStudy() {
+        return typeOfStudy;
+    }
+
+    public void setTypeOfStudy(String typeOfStudy) {
+        this.typeOfStudy = typeOfStudy;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getBorndate() {
+        return bornDate;
+    }
+
+    public void setBorndate(Date borndate) {
+        this.bornDate = bornDate;
+    }
+
+    @Override
+    public String toString() {
+        return "HomeSchoolStudent{" + "id=" + id + ", age=" + age + ", name=" + name + ", typeOfStudy=" + typeOfStudy + ", address=" + address + ", borndate=" + bornDate + '}';
+    }
+    
 }
