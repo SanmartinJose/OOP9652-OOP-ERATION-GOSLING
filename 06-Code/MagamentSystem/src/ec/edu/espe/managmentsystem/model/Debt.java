bd74b9c7651f6b66da82759ae4bc1ae7cddfdc81
SanmartinJose <132719688+SanmartinJose@users.noreply.github.com>
2023-06-01 03:41:50 -0500

    Reviewed and Fixed by Jose Sanmartin

--- 06-Code/MagamentSystem/src/ec/edu/espe/managmentsystem/model/Debt.java
+++ 06-Code/MagamentSystem/src/ec/edu/espe/managmentsystem/model/Debt.java
@@ -1,60 +1,13 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package ec.edu.espe.managmentsystem.model;
 
-import org.json.JSONObject;
 /**
  *
  * @author Sanmertin Jose, OOP-ERATION-GOSLING,DCC-ESPE
  */
-
 public class Debt {
-    private double value;
-    private double paid;
-    private double difference;
-
-    public Debt(double value, double paid) {
-        this.value = value;
-        this.paid = paid;
-        this.difference = value - paid;
-    }
-
-    public double getValue() {
-        return value;
-    }
-
-    public void setValue(double value) {
-        this.value = value;
-        updateDifference();
-    }
-
-    public double getPaid() {
-        return paid;
-    }
-
-    public void setPaid(double paid) {
-        this.paid = paid;
-        updateDifference();
-    }
-
-    public double getDifference() {
-        return difference;
-    }
-
-    private void updateDifference() {
-        this.difference = value - paid;
-    }
-
-    public JSONObject toJson() {
-        JSONObject json = new JSONObject();
-        json.put("value", value);
-        json.put("paid", paid);
-        json.put("difference", difference);
-        return json;
-    }
-
-    public static Debt fromJson(JSONObject json) {
-        double value = json.optDouble("value", 0.0);
-        double paid = json.optDouble("paid", 0.0);
-        return new Debt(value, paid);
-    }
+    
 }
