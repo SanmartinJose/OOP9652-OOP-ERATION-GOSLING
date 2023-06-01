7a548172182cd9c0a5a9393ee37254d8d010f2f1
Oswaldo <oswaldoj.tipan@gmail.com>
2023-06-01 01:24:01 -0500

    Menu in prosses

--- 06-Code/MagamentSystem/src/ec/edu/espe/managamentsystem/view/Menu.java
+++ 06-Code/MagamentSystem/src/ec/edu/espe/managamentsystem/view/Menu.java
@@ -9,30 +9,34 @@ import java.util.Scanner;
  * @author Michael Simbana, POO-ERATION-GOSLING, DCCO-ESPE
  */
 public class Menu {
+    
     public static void main(String[] args) {
+        
         PaymentRecord paymentRecord = new PaymentRecord("data/students.json", "data/debts.json");
-        Scanner scanner = new Scanner(System.in);
-
-        int opcion;
+        
+        Scanner optionIn = new Scanner(System.in);
+        int option;
+        
         do {
-            System.out.println("MENU");
-            System.out.println("1. Agregar deudas");
-            System.out.println("2. Actualizar pago");
-            System.out.println("3. Salir");
+            System.out.println("--------Sistema de administración de horarios-------");
+            System.out.println("1. Administrar cursos del HomeSchool");
+            System.out.println("2. Administrar estudiantes de Holística");
+            System.out.println("3. Administrar pagos");
+            System.out.println("4. Salir");
             System.out.print("Seleccione una opción: ");
-            opcion = scanner.nextInt();
+            option = optionIn.nextInt();
 
-            switch (opcion) {
+            switch (option) {
                 case 1:
-                    scanner.nextLine();
+                    optionIn.nextLine();
                     System.out.print("Ingrese el ID del estudiante: ");
-                    String id = scanner.nextLine();
+                    String id = optionIn.nextLine();
                     paymentRecord.monthlyValue(id);
                     break;
                 case 2:
-                    scanner.nextLine();
+                    optionIn.nextLine();
                     System.out.print("Ingrese el ID del estudiante: ");
-                    String id2 = scanner.nextLine();
+                    String id2 = optionIn.nextLine();
                     paymentRecord.updatePayment(id2);
                     break;
                 case 3:
@@ -42,6 +46,29 @@ public class Menu {
                     System.out.println("Opción inválida. Intente nuevamente.");
                     break;
             }
-        } while (opcion != 3);
+        } while (option != 3);
     }
+    
+    public void controlHomeSchoolMenu(){
+        
+        int option = 0;
+        Scanner optionIn = new Scanner(System.in);
+
+        do{
+            System.out.println("-------Administración del HomeSchool-----");
+            System.out.println("1. Administrar Estudiantes");
+            System.out.println("2. Administrar Profesores");
+            System.out.println("3. Aministar horarios");
+            System.out.println("4. Regresar");
+        }while(option!=3);
+    }
+    
+    public void controlHolisticStudentsMenu(){
+        
+    }
+    
+    public void controlPaymentsMenu(){
+        
+    }
+    
 }
